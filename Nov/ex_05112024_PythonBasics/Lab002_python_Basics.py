850e863616abaddda361c1d782b2b2749e468a60
Thiyagarajan <sthiyagarajan1307@gmail.com>
2024-11-08 22:34:04 +0530

    commit1

---
+++ Nov/ex_05112024_PythonBasics/Lab002_python_Basics.py
@@ -0,0 +1,4 @@
+print(2+2)
+print(2*2)
+print(4/4)
+print(1.2*1.2)
---
+++ Nov/ex_05112024_PythonBasics/Lab003_python_Basics.py
@@ -0,0 +1,4 @@
+print("Thiyagarajan", "Srinivasan", "Chennai", "QA Engineer")
+print("Thiyagarajan", "Srinivasan", "Chennai", "QA Engineer", sep="&")
+print("Thiyagarajan", "Srinivasan", "Chennai", "QA Engineer", end="__")
+print("B.E", 2020, "UCEK")
