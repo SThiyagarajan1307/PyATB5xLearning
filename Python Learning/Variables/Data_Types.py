239ca980f4b1de4f758d173dd3072c5664c5e6bd
Thiyagarajan Srinivasan <thiyagarajan.srinivasan@sathaga.com>
2024-11-15 18:59:21 +0530

    python variable learning

---
+++ Python Learning/Variables/Data_Types.py
@@ -0,0 +1,31 @@
+x1 = "Hello World"	#str
+print(x1)
+x2 = 20	#int
+print(x2)
+x3 = 20.5	#float
+print(x3)
+x4 = 1j	#complex
+print(x4)
+x5 = ["apple", "banana", "cherry"]	#list
+print(x5)
+x6 = ("apple", "banana", "cherry")	#tuple
+print(x6)
+x7 = range(6)	#range
+print(x7)
+x8 = {"name" : "John", "age" : 36}	#dict
+print(x8)
+x9 = {"apple", "banana", "cherry"} #set
+print(x9)
+x10 = frozenset({"apple", "banana", "cherry"})	#frozenset
+print(x10)
+x11 = True	#bool
+print(x11)
+x12 = b"Hello"	#bytes
+print(x12)
+x14 = bytearray(5)	#bytearray
+print(x14)
+x15 = memoryview(bytes(5))	#memoryview
+print(x15)
+x16 = None	#NoneType
+print(x16)
+
---
+++ Python Learning/Variables/PythonVariable.py
@@ -0,0 +1,14 @@
+x = "Python is awesome"
+print(x)
+
+a = "Python"
+b = "is"
+c = "awesome"
+print(a, b, c)
+
+x1 = "Python "
+y1 = "is "
+z1 = "awesome"
+print(x1 + y1 + z1)
+print("hello" + x1)
+print("{} {}".format("Hello", x1 + z1))
