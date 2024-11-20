5d0eda4774cdecbddb927b1cc76a223372f1a92d
Thiyagarajan Srinivasan <thiyagarajan.srinivasan@sathaga.com>
2024-11-20 14:00:02 +0530

    19112024 Lesson

--- Nov/ex_19112024_Functions/Lab073_Real_Args.py
+++ Nov/ex_19112024_Functions/Lab073_Real_Args.py
@@ -7,8 +7,9 @@ def make_pizza(*toppings):
     # for i in toppings:
     #     print(i)
 
-pramod = make_pizza("tomato","olives")
-jayati = make_pizza("pineapple","olives","corn","paneer")
+
+pramod = make_pizza("tomato", "olives")
+jayati = make_pizza("pineapple", "olives", "corn", "paneer")
 vinay = make_pizza("tomato")
 
 r1 = max(1, 2, 3, 4, 6)
